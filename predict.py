3034d46d6a424f4cbb319a1567a69f6f366546a4
Martin <chileshemartin@gmail.com>
2019-08-11 17:41:09 +0200

    style: Update predit.py

--- predict.py
+++ predict.py
@@ -2,7 +2,7 @@
 #
 # PROGRAMMER: Martin
 # DATE CREATED: 06/08/2019
-# REVISED DATE: 06/08/2019
+# REVISED DATE: 11/08/2019
 # PURPOSE: To predict the class of flowers the given input belongs to. 
 #          It uses the model trained from the train.py module
 #
