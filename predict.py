09068ef51d31c10f8c6cd81cd8bb6190646248ea
Martin <chileshemartin@gmail.com>
2019-08-14 09:21:00 +0200

    refactor: Update predict.py
    removed unnecessary import

--- predict.py
+++ predict.py
@@ -2,7 +2,7 @@
 #
 # PROGRAMMER: Martin
 # DATE CREATED: 06/08/2019
-# REVISED DATE: 12/08/2019
+# REVISED DATE: 13/08/2019
 # PURPOSE: To predict the class of flowers the given input belongs to. 
 #          It uses the model trained from the train.py module
 #
